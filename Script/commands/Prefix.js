a6c399932e2e407af3238047bcae5ec8b5886149
mashikmahir185-cyber <mashikmahir185@gmail.com>
2025-09-12 17:33:33 +0600

    Update Prefix.js

--- Script/commands/Prefix.js
+++ Script/commands/Prefix.js
@@ -31,7 +31,7 @@ module.exports.handleEvent = async ({ event, api, Threads }) => {
   if (triggerWords.includes(lowerBody)) {
     return api.sendMessage(
 `🌟━━━━━━━━━━━━━━━━━🌟
-　『 𝐏𝐑𝐄𝐅𝐈𝐗 					𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 』
+　『 𝐏𝐑𝐄𝐅𝐈𝐗 					\n					𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 』
 🌟━━━━━━━━━━━━━━━━━🌟
 『 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 』
 
